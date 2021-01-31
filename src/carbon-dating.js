47ca7513d0be8d89c60e79f55a422b8a461a5abd
Kianeay <70838054+Kianeay@users.noreply.github.com>
2021-01-31 19:08:17 +0300

    init: start ccarbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -4,6 +4,14 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  const modernActivity =15;
+  const halfLifePeriod = 5730;
+  if (sampleActivity === 'no-value') return false;
+  if (typeof sampleActivity !== 'string') return false;
+  const sampleActivityNumeric = parseFloat(sampleActivity);
+  if (isNaN(sampleActivityNumeric)) return false;
+  if (sampleActivityNumeric > modernActivity) return false;
+  if (sampleActivityNumeric === 0 || sampleActivityNumeric < 0) return false;
+  const k = Math.LN2 / halfLifePeriod;
+  return Math.ceil(Math.log(modernActivity / sampleActivityNumeric) / k);
 };
