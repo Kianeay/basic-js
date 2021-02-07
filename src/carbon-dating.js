022020f39b3ca14136e85ba39266008e1fef8705
Kianeay <70838054+Kianeay@users.noreply.github.com>
2021-02-07 12:25:28 +0300

    feat: implement count cats

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,7 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
+module.exports = function dateSample(sampleActivity) {
   const modernActivity =15;
   const halfLifePeriod = 5730;
   if (sampleActivity === 'no-value') return false;
